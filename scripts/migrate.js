5400d88880169dd5923d6d997f0c2bcd425e5a60
Olivier Sarrouy <olivier.sarrouy@gmail.com>
2020-08-19 16:42:43 +0200

    Clean migration script

--- scripts/migrate.js
+++ scripts/migrate.js
@@ -1,19 +1,20 @@
 const DAOstackMigration = require('@daostack/migration');
-require('dotenv').config();
-
+const bre = require('@nomiclabs/buidler');
 const specs = require('./necDAO.json');
-const proxy = require('../deployments/rinkeby/UniswapProxy.json');
 
 const migrate = async () => {
+  const { log, get } = bre.deployments;
+
+  const proxy = await get('UniswapProxy');
   specs.CustomSchemes[0].address = proxy.address;
   specs.CustomSchemes[1].params[2] = proxy.address;
 
   const options = {
-    network: 'rinkeby',
-    provider: 'http://127.0.0.1:1248',
-    privateKey: process.env.KEY,
-    customAbisLocation: '../contracts/artifacts',
-    quiet: true,
+    network: bre.network.name,
+    provider: bre.network.config.url,
+    privateKey: process.env.KEY || '242d567b6917dce8f836dc1fb190259c65998d38b14dddf2b94f45f41bb9fd19',
+    customAbisLocation: './artifacts',
+    quiet: false,
     force: true,
     restart: true,
     params: {
@@ -22,7 +23,7 @@ const migrate = async () => {
   };
 
   const result = await DAOstackMigration.migrateDAO(options);
-  console.log('+ Deployed DAO at ' + result.dao['0.0.1-rc.44'].Avatar);
+  log('+ Deployed DAO at ' + result.dao['0.0.1-rc.44'].Avatar);
 };
 
 migrate();

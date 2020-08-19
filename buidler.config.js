1ae94c7105a67c380fa4f2e88f3fd2e5d5dab9cf
Olivier Sarrouy <olivier.sarrouy@gmail.com>
2020-08-19 16:12:08 +0200

    Update deploy scripts

--- buidler.config.js
+++ buidler.config.js
@@ -7,18 +7,21 @@ usePlugin('solidity-coverage');
 module.exports = {
   solc: {
     version: '0.5.13',
-    optimizer: { enabled: true, runs: 10 },
+    optimizer: { enabled: true, runs: 1 },
   },
   gasReporter: {
     enabled: process.env.REPORT_GAS ? true : false,
   },
   namedAccounts: {
     deployer: {
-      default: 0, // here this will by default take the first account as deployer
-      4: '0xb71d2d88030a00830c3d45f84c12cc8aaf6857a5', // but for rinkeby it will be a specific address
+      default: 0,
+      4: '0xb71d2d88030a00830c3d45f84c12cc8aaf6857a5',
     },
   },
   networks: {
+    private: {
+      url: 'http://127.0.0.1:8545',
+    },
     rinkeby: {
       url: 'http://127.0.0.1:1248',
     },
